984546888b0a7c01605f42f2a71e9bf931784d81
rovshan <rouvsen@gmail.com>
2023-11-12 20:10:23 +0400

    Add :True,False,None conventions to chatper-1

---
+++ chapter-1/true_false_none.py
@@ -0,0 +1,18 @@
+print(False == 0)
+print(True == 1)
+ 
+print(True + True + True)
+print(True + False + False)
+ 
+print(None == 0)
+print(None == [])
+
+"""
+Output:
+True
+True
+3
+1
+False
+False
+"""

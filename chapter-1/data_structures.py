771a75945b9dab41ef9c9f53f64f2082e17009cd
rovshan <rouvsen@gmail.com>
2023-11-12 19:56:09 +0400

    Add data structures in python to chapter-1

---
+++ chapter-1/data_structures.py
@@ -0,0 +1,18 @@
+# list
+arr = [1,2,3,"Hello"]
+print("This is list (arr)", arr)
+
+# dict (dictionary)
+dict = {
+    "username": "rouvsen",
+    "password": "pass"
+}
+print("This is dict", dict)
+
+# tuple
+tuple = (1,2,3,4,5,5)
+print("This is tuple", tuple)
+
+# set
+mySet = set([1,2,3,4,5,5,5,5])
+print("This is set", mySet)
